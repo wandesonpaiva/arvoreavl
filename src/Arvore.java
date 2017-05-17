f918f31dbe652a9e7633e2982ea82272a26dd4b2
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2017-05-17 12:41:45 -0300

    conclusão iniserção, inicio remoção

--- src/Arvore.java
+++ src/Arvore.java
@@ -9,34 +9,34 @@ public class Arvore {
     	System.out.println(valor + " inserido na raiz");
     	raiz = no;
     } else {
-      No pai = acharPai(valor, raiz);
+    	No pai = acharPai(valor, raiz);
 		if (pai != null){
-		  no.setPai(pai);
-		  if (pai.getValor() > valor){
-			  System.out.println(valor + " inserido a esquerda de " + pai.getValor());
-			  pai.setEsquerdo(no);
-		  } else if(pai.getValor() < valor){
-			  System.out.println(valor + " inserido a direita de " + pai.getValor());
-			  pai.setDireito(no);
-		  }
-		  ajustarFbInsercao(no);
+			no.setPai(pai);
+			if (pai.getValor() > valor){
+				System.out.println(valor + " inserido a esquerda de " + pai.getValor());
+				pai.setEsquerdo(no);
+			} else if(pai.getValor() < valor){
+				System.out.println(valor + " inserido a direita de " + pai.getValor());
+				pai.setDireito(no);
+			}
+			ajustarFbInsercao(no);
       	} else {
-  		  System.out.println("valor já existe");
+      		System.out.println("valor já existe");
       	}
     }
   }
 
   // ACHAR PAI
   public No acharPai(int valor, No no){
-    if (valor < no.getValor() && no.getEsquerdo() != null){
-      return acharPai(valor, no.getEsquerdo());
-    } else if(valor > no.getValor() && no.getDireito() != null){
-      return acharPai(valor, no.getDireito());
-    } else if(valor == no.getValor()) {
-    	return null;
-    } else {
-      return no;
-    }
+	  if (valor < no.getValor() && no.getEsquerdo() != null){
+		  return acharPai(valor, no.getEsquerdo());
+	  } else if(valor > no.getValor() && no.getDireito() != null){
+		  return acharPai(valor, no.getDireito());
+	  } else if(valor == no.getValor()) {
+		  return null;
+	  } else {
+		  return no;
+	  }
   }
 
   // AJUSTAR FB NA INSERÇÃO
@@ -48,34 +48,34 @@ public class Arvore {
     
     if (pai != null && pai.getEsquerdo() != null && pai.getEsquerdo().getValor() == filho.getValor()){
       //System.out.println("Antigo FB de " + pai.getValor() + " é: " + pai.getFb());
-      pai.setFb(pai.getFb() + 1);
-      System.out.println("Novo FB de " + pai.getValor() + " é: " + pai.getFb());
-	  if (pai.getFb() >= 2){
-	    if (pai.getEsquerdo().getFb() < 0){
-	      System.out.println("RotacaoDireitaDupla em " + pai.getValor());
-	      rotacaoDireitaDupla(pai);
-	    } else {
-	      System.out.println("RotacaoDireitaSimples em " + pai.getValor());
-	      rotacaoDireitaSimples(pai);
-	    }
-	  } else if(pai.getFb() != 0) {
-		ajustarFbInsercao(pai);
-	  }
+    	pai.setFb(pai.getFb() + 1);
+    	System.out.println("Novo FB de " + pai.getValor() + " é: " + pai.getFb());
+    	if (pai.getFb() >= 2){
+    		if (pai.getEsquerdo().getFb() < 0){
+    			System.out.println("RotacaoDireitaDupla em " + pai.getValor());
+    			rotacaoDireitaDupla(pai);
+    		} else {
+    			System.out.println("RotacaoDireitaSimples em " + pai.getValor());
+    			rotacaoDireitaSimples(pai);
+    		}
+    	} else if(pai.getFb() != 0) {
+    		ajustarFbInsercao(pai);
+    	}
     } else if(pai != null && pai.getDireito() != null && pai.getDireito().getValor() == filho.getValor()){
       //System.out.println("Antigo FB de " + pai.getValor() + " é: " + pai.getFb());
-      pai.setFb(pai.getFb() - 1);
-      System.out.println("Novo FB de " + pai.getValor() + " é: " + pai.getFb());
-      if (pai.getFb() <= -2){
-        if (pai.getDireito().getFb() > 0){
-          System.out.println("RotacaoEsquerdaDupla em " + pai.getValor());
-          rotacaoEsquerdaDupla(pai);
-        } else{
-          System.out.println("RotacaoEsquerdaSimples em " + pai.getValor());
-          rotacaoEsquerdaSimples(pai);
-        }
-      } else if(pai.getFb() != 0){
-    	ajustarFbInsercao(pai);  
-      }
+    	pai.setFb(pai.getFb() - 1);
+    	System.out.println("Novo FB de " + pai.getValor() + " é: " + pai.getFb());
+    	if (pai.getFb() <= -2){
+    		if (pai.getDireito().getFb() > 0){
+    			System.out.println("RotacaoEsquerdaDupla em " + pai.getValor());
+    			rotacaoEsquerdaDupla(pai);
+    		} else{
+    			System.out.println("RotacaoEsquerdaSimples em " + pai.getValor());
+    			rotacaoEsquerdaSimples(pai);
+    		}
+    	} else if(pai.getFb() != 0){
+    		ajustarFbInsercao(pai);  
+    	}
     }
   }
   
@@ -118,8 +118,8 @@ public class Arvore {
 	  }
 	  
 	  if (filho.getDireito() != null){
-		filho.getDireito().setPai(pai);
-		pai.setEsquerdo(filho.getDireito());
+		  filho.getDireito().setPai(pai);
+		  pai.setEsquerdo(filho.getDireito());
 	  }
 	  
 	  filho.setDireito(pai);
@@ -149,35 +149,80 @@ public class Arvore {
   // REMOVER
   public void remover(int valor){
       No aRemover = acharNo(raiz, valor);
+      No pai = aRemover.getPai();
+      
       if (aRemover != null){
-
+		  /* Primeiro caso: aRemover nao tem filhos */
+    	  if (aRemover.getEsquerdo() == null && aRemover.getDireito() == null){
+    		  if (aRemover.getValor() == raiz.getValor()){
+    			  raiz = null;
+    		  } else {
+    			  aRemover.setPai(null);
+        		  if (aRemover.getValor() == pai.getEsquerdo().getValor()){
+        			  pai.setEsquerdo(null);
+        		  } else if (aRemover.getValor() == pai.getDireito().getValor()){
+        			  pai.setDireito(null);
+        		  }
+    		  }
+    	  }
+		  /* Segundo caso: aRemover so tem o filho esquerdo */
+    	  if (aRemover.getEsquerdo() != null && aRemover.getDireito() == null){
+    		  if (aRemover.getValor() == raiz.getValor()){
+    			  raiz = aRemover.getEsquerdo();
+    			  aRemover.getEsquerdo().setPai(null);
+    		  } else {
+    			  aRemover.setPai(null);
+    			  aRemover.getEsquerdo().setPai(pai);
+    			  if (aRemover.getValor() == pai.getEsquerdo().getValor()){
+        			  pai.setEsquerdo(aRemover.getEsquerdo());
+        		  } else if (aRemover.getValor() == pai.getDireito().getValor()){
+        			  pai.setDireito(aRemover.getEsquerdo());
+        		  }
+    		  }
+    	  }
+		  /* Terceiro caso: aRemover so tem o filho direito */
+    	  if (aRemover.getEsquerdo() == null && aRemover.getDireito() != null){
+    		  if (aRemover.getValor() == raiz.getValor()){
+    			  raiz = aRemover.getEsquerdo();
+    			  aRemover.getDireito().setPai(null);
+    		  } else {
+    			  aRemover.setPai(null);
+    			  aRemover.getDireito().setPai(pai);
+    			  if (aRemover.getValor() == pai.getEsquerdo().getValor()){
+        			  pai.setEsquerdo(aRemover.getDireito());
+        		  } else if (aRemover.getValor() == pai.getDireito().getValor()){
+        			  pai.setDireito(aRemover.getDireito());
+        		  }
+    		  }
+    	  }
+		  /* Quarto caso: aRemover tem dois filhos */
       } else {
-        System.out.println("valor a remover não encontrado");
+    	  System.out.println("valor a remover não encontrado");
       }
   }
 
   // ACAHAR NÓ
   public No acharNo(No no, int valor){
-    if(no.getValor() == valor){
-      return no;
-    } else if(no.getValor() < valor && no.getDireito() != null) {
-      return acharNo(no.getDireito(), valor);
-    } else if(no.getValor() > valor && no.getEsquerdo() != null){
-      return acharNo(no.getEsquerdo(), valor);
-    } else {
-      return null;
-    }
+	  if(no.getValor() == valor){
+		  return no;
+	  } else if(no.getValor() < valor && no.getDireito() != null) {
+		  return acharNo(no.getDireito(), valor);
+	  } else if(no.getValor() > valor && no.getEsquerdo() != null){
+		  return acharNo(no.getEsquerdo(), valor);
+	  } else {
+		  return null;
+	  }
   }
 
   // IN ORDER
   public void inOrder(No no){
-    if (no.getEsquerdo() != null){
-      inOrder(no.getEsquerdo());
-    }
-    System.out.println(no.getValor());
-    if (no.getDireito() != null){
-      inOrder(no.getDireito());
-    }
+	  if (no.getEsquerdo() != null){
+		  inOrder(no.getEsquerdo());
+	  }
+	  System.out.println(no.getValor() + " (" + no.getFb() + ")");
+	  if (no.getDireito() != null){
+		  inOrder(no.getDireito());
+	  }
   }
 
 }

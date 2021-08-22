5aa3b490990d4d43d0d9304007eab99aa7ebb525
wandeson paiva <wandeson.jpaiva@gmail.com>
2021-08-22 12:32:00 -0300

    ajusta classe aux

--- src/Aux.java
+++ src/Aux.java
@@ -8,21 +8,9 @@ public class Aux{
     avl.inserir(90);
     avl.inserir(44);
     avl.inserir(65);
-    /*avl.inserir(22);
-    avl.inserir(36);
-    avl.inserir(78);
-    avl.inserir(84);
-    avl.inserir(11);
-    avl.inserir(2);
-    avl.inserir(19);*/
-    
     
     avl.remover(9);
-    //avl.inserir(4);
-    //avl.inserir(5);    
     System.out.println("--- in ordem ---");
     avl.inOrder(avl.raiz);
-
   }
-
 }
